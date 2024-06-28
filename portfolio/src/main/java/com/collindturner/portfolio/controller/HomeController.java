8396e162acdeb58afe0a8d5460f556d340673c32
collin <collin.turn@gmail.com>
2024-06-28 00:20:58 -0400

    add home page building blocks for ide display

---
+++ portfolio/src/main/java/com/collindturner/portfolio/controller/HomeController.java
@@ -0,0 +1,27 @@
+
+/*
+ *******************************************************************************
+ * Project: Portfolio Website
+ *
+ * Author: Collin Turner
+ * =============================================================================
+ * (c) Copyright 2024 CollinDTurner All rights reserved.
+ *******************************************************************************
+*/
+package com.collindturner.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping({"", "/"})
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
+}
